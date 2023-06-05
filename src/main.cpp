dc515cd8321cdab43da4d705c4f3c31d3b12598d
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-06-06 00:07:03 +0200

    reprogram i2c

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,43 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com
-*********/
-
 #include <Wire.h>
 #include <Arduino.h>
+
+#define BMP280_ADDRESS 0x77 // Current address of the BMP280 sensor
 
 void setup()
 {
   Wire.begin();
   Serial.begin(115200);
-  Serial.println("\nI2C Scanner");
+  delay(1000);
+
+  // Open serial monitor to check the output
+  Serial.println("Starting BMP280 address reprogramming...");
 }
 
 void loop()
 {
-  byte error, address;
-  int nDevices;
-  Serial.println("Scanning...");
-  nDevices = 0;
-  for (address = 1; address < 127; address++)
+  // Send the address programming command sequence
+  Wire.beginTransmission(BMP280_ADDRESS);
+  Wire.write(0xf5); // Register address of CTRL_REG1
+  Wire.write(0x76); // New address to be programmed (0x76)
+  Wire.endTransmission();
+
+  delay(100); // Delay to allow the sensor to process the address change
+
+  // Verify the address change
+  Wire.beginTransmission(0x76);
+  byte error = Wire.endTransmission();
+
+  if (error == 0)
   {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address < 16)
-      {
-        Serial.print("0");
-      }
-      Serial.println(address, HEX);
-      nDevices++;
-    }
-    else if (error == 4)
-    {
-      Serial.print("Unknow error at address 0x");
-      if (address < 16)
-      {
-        Serial.print("0");
-      }
-      Serial.println(address, HEX);
-    }
-  }
-  if (nDevices == 0)
-  {
-    Serial.println("No I2C devices found\n");
+    Serial.println("BMP280 address reprogrammed successfully!");
   }
   else
   {
-    Serial.println("done\n");
+    Serial.println("Error occurred during BMP280 address reprogramming!");
   }
-  delay(5000);
+
+  while (1)
+  {
+    // Stay in an infinite loop after reprogramming the address
+  }
 }
